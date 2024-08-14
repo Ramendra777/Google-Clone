615c11db33651947ef7f1fffb1852a7b1a00fa78
Ramendra777 <ramendra335428@gmail.com>
2024-08-15 05:13:21 +0530

    Added pagination with some changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,14 @@
 import axios from "axios";
+// import dotenv from "dotenv";
+
+// // Load environment variables from .env file
+// dotenv.config();
 
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 
 const params = {
-    key: "YOUR_API_KEY",
-    cx: "YOUR_CX_KEY",
+    key: "AIzaSyDhxBORdogQz92F9HDoI84J4454W2qht40", // Ensure correct environment variable naming
+    cx: "e5a60857b049c47d4",
 };
 
 export const fetchDataFromApi = async (payload) => {
